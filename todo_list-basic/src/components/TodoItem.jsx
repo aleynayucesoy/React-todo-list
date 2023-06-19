96faa91a4fa50ad2110203345bedbd234b60ddc0
Aleyna Yücesoy <aleyyucesoy@gmail.com>
2023-06-19 15:12:45 +0300

    basic todo initial commit

---
+++ todo_list-basic/src/components/TodoItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { toast } from 'react-toastify'
+
+const TodoItem = ({ todo, deleteTodo, id }) => {
+    return (
+        <div className='todo-item'>
+            <p> {todo} </p>
+            <i className="fas fa-times-circle"
+                onClick={() => {
+                    deleteTodo(id)
+                    toast.error("Todo Deleted!")
+                }}
+
+            ></i>
+        </div>
+    )
+}
+
+export default TodoItem
